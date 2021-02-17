178a0b5b8ee18550240819d6d7e642344528bbb3
pcrantfo <79102975+pcrantfo@users.noreply.github.com>
2021-02-17 10:04:49 -0700

    Debug

--- stylelint.config.js
+++ stylelint.config.js
@@ -20,7 +20,7 @@ module.exports = {
     "no-extra-semicolons": true,
     "no-invalid-double-slash-comments": true,
     "property-no-unknown": true,
-    "property-no-vendor-prefix": false
+    "property-no-vendor-prefix": false,
     "selector-pseudo-class-no-unknown": true,
     "selector-pseudo-element-no-unknown": true,
     "selector-type-no-unknown": true,
